cb60adc20630426c793e2964fe916d813e954f18
Szabolcs Berkó <szberko@gmail.com>
2018-04-16 23:05:25 +0200

    including header and main component into App.js

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
+import {Header} from './Header';
+import {Main} from './Main';
 import './../scss/main.scss';
 
 export default class App extends Component {
     render () {
         return (
-            <div className="shelter-container"></div>
-
+            <div className="main-container">
+                <Header/>
+                <Main/>
+            </div>
         );
     }
 }
