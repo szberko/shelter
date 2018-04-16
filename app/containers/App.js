261e214df787ed9246ea1b2ead5ee3684c1a410b
Szabolcs Berkó <szberko@gmail.com>
2018-04-16 21:19:45 +0200

    add image to the background

--- app/containers/App.js
+++ app/containers/App.js
@@ -3,6 +3,9 @@ import './../scss/main.scss';
 
 export default class App extends Component {
     render () {
-        return <p>This is my new react app.</p>
+        return (
+            <div className="shelter-container"></div>
+
+        );
     }
 }
