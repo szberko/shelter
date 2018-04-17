4b0f4cc1f34ae2c4855e01552ac978fbd8ab0734
Szabolcs Berkó <szberko@gmail.com>
2018-04-17 13:39:49 +0200

    introduce tabbed view

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -1,19 +1,29 @@
 import React, {Component} from 'react';
+import {Animal} from './Animal';
+import ReactDOM from 'react-dom';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.scss';
 
 export class Main extends Component{
-    render
-    () {
+    render() {
         return (
             <div className="main-container__body">
                 <div className="body__menu">
-                    <ul id="navigation">
-                        <li>Gazdára vár</li>
-                        <li>Örökbeadott</li>
-                    </ul>
+                <Tabs selectedTabPanelClassName="body__content">
+                    <TabList>
+                        <Tab>Gazdára vár</Tab>
+                        <Tab>Örökbeadott</Tab>
+                    </TabList>
+                
+                    <TabPanel>
+                        <Animal/>
+                        <Animal/>
+                    </TabPanel>
+                    <TabPanel>
+                        <Animal/>
+                    </TabPanel>
+                </Tabs>
                     <div id="filter">Szűrő</div>
-                </div>
-                <div className="body__content">
-                    
                 </div>
             </div>
         );
