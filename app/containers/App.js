5316812e902a815f10ccbe6da97fac4796187925
Szabolcs Berkó <szberko@gmail.com>
2018-04-19 00:27:12 +0200

    add Footer component

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Header} from './Header';
 import {Main} from './Main';
+import {Footer} from './Footer';
 import './../scss/main.scss';
 
 export default class App extends Component {
@@ -9,6 +10,7 @@ export default class App extends Component {
             <div className="main-container">
                 <Header/>
                 <Main/>
+                <Footer/>
             </div>
         );
     }
---
+++ app/containers/Footer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class Footer extends Component {
+    render () {
+        return (
+            <div className="main-container__footer">
+                
+            </div>
+        );
+    }
+}
