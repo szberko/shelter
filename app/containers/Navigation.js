ba07641864de3c7f78c973896323585e96214b5f
Szabolcs Berkó <szberko@gmail.com>
2018-04-19 14:12:54 +0200

    remove unnecessary code

--- app/containers/Navigation.js
+++ app/containers/Navigation.js
@@ -1,14 +1,6 @@
 import React, {Component} from 'react';
 
 export class Navigation extends Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-
-        }
-    }
-
     render() {
         return (
             <div className="navbar">
